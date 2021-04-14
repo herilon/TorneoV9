7075d612a7c8497234e0fd4421f878c5ec77c578
Heri <hlondono@misena.edu.com>
2021-04-14 18:48:05 -0500

    Actividad 11 Formularios

--- resources/views/equipos/create.blade.php
+++ resources/views/equipos/create.blade.php
@@ -1,6 +1,28 @@
 @extends('layouts.base')
 @section('title', 'Torneo')
 @section('content')
-    <h1>Crear equipo</h1>   
+	<form action="/equipos" method="post" enctype="multipart/form-data">
+		@csrf
+		<div class="form-group">
+			<label for="nombre">Nombre</label>
+			<input type="text" name="nombre" id="nombre" class="form-control">
+		</div>
+		<div class="form-group">
+			<label for="dt">D.T.</label>
+			<input type="text" name="dt" id="dt" class="form-control">
+		</div>
+		<div class="form-group">
+    	<label for="municipio">Municipo</label>
+	    <select class="form-control" id="municipio" name="municipio">
+            <option value="1">Manizales</option>
+            <option value="2">Pereira</option>
+            <option value="3">Armenia</option>
+        </select>
+  	</div>
+		<div class="form-group">
+			<label for="escudo">Escudo</label>
+			<input type="file" name="escudo" id="escudo" class="form-control-file">
+		</div>
+		<button type="submit" class="btn btn-primary">Crear</button>
+	</form>
 @endsection
-
--- resources/views/equipos/show.blade.php
+++ resources/views/equipos/show.blade.php
@@ -8,7 +8,7 @@
         <div class="col-sm-8">
             <p class="h3">{{ $equipo['nombre'] }}</p>
             <p class="h5">{{ $equipo['dt'] }}</p>
-            <p class="h5">{{ $equipo['pais'] }}</p>
+            <p class="h5">{{ $equipo['municipio'] }}</p>
             <a href="#" class="btn btn-danger">Eliminar</a>
             <a href="/equipos/{{ $id }}/edit" class="btn btn-warning">Modificar</a>
             <a href="/equipos" class="btn btn-outline-dark">Ver equipos</a>
