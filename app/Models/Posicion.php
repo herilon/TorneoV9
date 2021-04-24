d9541efddf65c0baae9645804b5a0a2fbd7a9c4c
Heri <hlondono@misena.edu.com>
2021-04-24 11:55:42 -0500

    Actividad 15 a 17 Modelos

---
+++ app/Models/Posicion.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+class Posicion extends Model
+{
+    use HasFactory;
+    protected $table = 'posiciones';
+    public function Jugadores()
+    {
+        return $this->hasMany(Jugador::class);
+    }
+}
