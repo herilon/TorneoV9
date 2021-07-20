0b40ea32bec42345014c05709f0047f03fb08e37
Heri <hlondono@misena.edu.com>
2021-07-19 23:19:28 -0500

    Tailwind instalado

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	<title>Torneo - @yield('title')</title>
+  </head>
+  <body>
+    <div class="container">
+        @include('partials.navbar')
+        @yield('content')
+    </div>
+  </body>
+</html>
+
