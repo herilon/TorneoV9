2e31c935905c389835ba3d0a880b9491dc39e26f
Heri <hlondono@misena.edu.com>
2021-07-31 23:29:34 -0500

    login full

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.base')
+@section('title', 'login')
+@section('content')
+    @if($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form action="{{ route('login') }}" method="post">
+        @csrf
+        <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+@stop
